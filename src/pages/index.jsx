d9d0d471cc72741713d92ec4178964cf4292b085
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-04-03 19:50:02 +0530

    feat: Initial Commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background: red;
+`;
+
+const HomePage = () => (
+  <Container>
+    <h1>Home Page</h1>
+  </Container>
+);
+
+export default HomePage;
