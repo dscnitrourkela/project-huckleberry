f91412c1d292cdb26dd2532900774ff7f4c1a40a
sakshi <ayushmanj138@gmail.com>
2023-07-08 13:07:21 +0530

    fix:Button

--- src/components/atoms/shared/button/Button.jsx
+++ src/components/atoms/shared/button/Button.jsx
@@ -1,15 +1,116 @@
-import { Light, StyledButton } from './Buttonstyles';
 
-function Button({ method = undefined }) {
+import styled from "styled-components";
+
+
+const Buttonstyles = styled.button`
+    background-color: ${(props) =>
+	 props.variant === "outline"
+	  ? "white"
+	   : props.variant === "disabled" 
+	   ?"#A6A6A6"  
+	   : "#145EF1"}; 
+   color :${(props) =>
+	 props.variant === "outline"
+	  ? "#145EF1"
+	   : props.variant === "disabled" 
+	   ?"#fff"  
+	   : "#fff"} ;
+	   border-radius: 8px;
+	   border: ${(props) =>
+	 props.variant === "outline"
+	  ? "2px solid #145EF1"
+	   : props.variant === "disabled" 
+	   ?"none"
+	   : "#fff"}; ;
+
+display: inline-flex;
+height: 60px;
+padding: 12px 48px;
+justify-content: center;
+align-items: center;
+flex-shrink: 0;
+    
+    font-size: 16px;
+    font-weight: 600;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+        border-radius: 8px;
+display: inline-flex;
+height: 60px;
+padding: 12px 48px;
+justify-content: center;
+align-items: center;
+flex-shrink: 0;
+background-color: ${(props) =>
+	 props.variant === "outline"
+	  ? "#F7F4F4"
+	   : props.variant === "disabled" 
+	   ?"#D7CDCD"  
+	   : "#558BF5"}; 
+
+    }
+	
+     &:focus {
+        outline: none;
+		color: #145EF1;
+		background-color: #fff;
+		border: 2px solid #145EF1;
+		border-radius: 8px;
+		display: inline-flex;
+height: 60px;
+padding: 12px 48px;
+justify-content: center;
+align-items: center;
+flex-shrink: 0;
+
+
+
+		}
+    
+	&:active {
+		border-radius: 8px;
+display: inline-flex;
+height: 60px;
+padding: 12px 48px;
+justify-content: center;
+align-items: center;
+flex-shrink: 0;
+
+	}
+	}
+
+	@media (max-width: 850px) {
+  
+		font-size: 14px;
+		font-weight: 600;
+		display: inline-flex;
+height: 44px;
+padding: 8px 32px;
+justify-content: center;
+align-items: center;
+gap: 10px;
+flex-shrink: 0;
+border-radius: 4px;
+  }
+	
+`;
+
+const Button = ({ type, variant, onclick, children, size}) => {
 	return (
-		<>
-			<StyledButton>button</StyledButton>
-			<StyledButton variant='outline' onClick={method}>
-				button
-			</StyledButton>
-			<Light>disable</Light>
-		</>
+		<Buttonstyles type={type} variant={variant} onClick={onclick} size={size} >
+
+			{children}
+    
+		</Buttonstyles>
 	);
-}
+};
+  
+        
+           
+    
+    
+
 
 export default Button;
--- src/pages/playground.jsx
+++ src/pages/playground.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+// import Button from '../atoms/shared/button/Button';
+// import Link from 'next/link';
 
 const Playground = () => {
 	const [value, setValue] = useState(0);
@@ -8,6 +10,12 @@ const Playground = () => {
 			<h1>Playground</h1>
 			<p>Value: {value}</p>
 			<button onClick={() => setValue(value + 1)}>Increment</button>
+			{/* <Link href="/" passHref>
+		<Button variant="disabled">Button</Button>
+		</Link>
+		<Button>Button</Button>
+		<Button variant="outline">Button</Button>
+		<Button variant="disabled">Button</Button> */}
 		</div>
 	);
 };
