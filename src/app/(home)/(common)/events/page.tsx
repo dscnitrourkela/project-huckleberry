6b7faf7e357b7535056373951cd4ded5044f924c
Harsh <harshtanishq2002@gmail.com>
2025-05-09 18:41:50 +0530

    chore: code cleanup

--- src/app/(home)/(common)/events/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from 'next';
-import { getAllEvents } from '@/actions/events';
-import { eventsMetadata } from '@/config/seo/events-metadata';
-
-export const metadata: Metadata = eventsMetadata;
-
-export default function EventsPage() {
-  return <div></div>;
-}
